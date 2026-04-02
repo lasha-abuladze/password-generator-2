954e479ad4f5340bf31fcee2e0d069cd53aa013e
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-04-02 23:34:48 +0400

    feat: implement dynamic character length update

--- app/script.js
+++ app/script.js
@@ -2,12 +2,15 @@
 
 const inpRange = document.querySelector(`#inp-range`);
 
+const spanCharacterLength = document.querySelector(`.span--character-length`);
+
 const updateSlider = function () {
     const value = (inpRange.value - inpRange.min) / (inpRange.max - inpRange.min) * 100;
     inpRange.style.background = `linear-gradient(to right, #A4FFAF 0%, #A4FFAF ${value}%, #18171F ${value}%, #18171F 100%)`;
+
+    spanCharacterLength.textContent = `${inpRange.value}`
 }
 
 inpRange.addEventListener(`input`, updateSlider);
 
-// Important: Run it once so it's correct when the page first loads!
 updateSlider();
