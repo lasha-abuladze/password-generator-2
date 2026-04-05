b276a5c61913435f60af08089093026c3f2e244b
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-04-05 15:44:35 +0400

    feat: implement password generator class and slider logic

--- app/script.js
+++ app/script.js
@@ -1,3 +1,191 @@
+// `use strict`;
+
+// const inpRange = document.querySelector(`#inp-range`);
+
+// const spanCharacterLength = document.querySelector(`.span--character-length`);
+
+// const inpCheckboxArr = Array.from(document.querySelectorAll(`.inp-checkbox`));
+// const btnSubmit = document.querySelector(`.btn--generate-password`);
+
+// const passwordText = document.querySelector(`.password`);
+
+
+
+
+// let characterTypes = [];
+// let passwordLength;
+
+// let passwordLetters = [];
+
+// let password;
+
+// let passwordIsGenerated = false
+// let usedFunctions= [];
+
+
+
+
+
+// const updateSlider = function () {
+//     const value = (inpRange.value - inpRange.min) / (inpRange.max - inpRange.min) * 100;
+//     inpRange.style.background = `linear-gradient(to right, #A4FFAF 0%, #A4FFAF ${value}%, #18171F ${value}%, #18171F 100%)`;
+//     passwordLength = inpRange.value;
+
+//     spanCharacterLength.textContent = `${passwordLength}`
+// }
+
+
+
+// inpRange.addEventListener(`input`, updateSlider);
+
+// updateSlider();
+
+
+//// generates random symbol
+// const getRandomSymbol = () => {
+
+//     let s = `s`;
+//     usedFunctions.push(s)
+//   const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+//   const randomIndex = Math.floor(Math.random() * symbols.length);
+  
+//   return symbols[randomIndex];
+// };
+
+///// generates random number
+// const getRandomNumber = () => {
+//     let n = `n`;
+//     usedFunctions.push(n)
+//     return Math.floor(Math.random() * 10)
+// };
+
+
+/// generates random letter
+// const getRandomLowercaseLetter = () => {
+//     let l = `l`;
+//     usedFunctions.push(l)
+//   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+//   return alphabet[Math.floor(Math.random() * alphabet.length)];
+// };
+
+
+// ///generate random uppercase letter 
+// const getRandomUppercaseLetter = () => {
+//     let u = `u`;
+//     usedFunctions.push(u)
+//   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+//   return alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
+// };
+
+
+
+// const  functions = [getRandomUppercaseLetter, getRandomLowercaseLetter, getRandomNumber, getRandomSymbol]
+
+
+
+
+// btnSubmit.addEventListener(`click`, function(e) {
+//     e.preventDefault();
+
+//     if(passwordLength < 8) {
+//         console.log(`bye`)
+//     } else {
+
+//         const inpIsChewcked = inpCheckboxArr.some(el => el.checked);
+
+//         if(!inpIsChewcked) {
+//             console.log(`not checked`)
+//         } else {
+//             characterTypes = [];
+//             passwordLetters = [];
+//             password = ``;
+//             passwordIsGenerated = false;
+//             usedFunctions = [];
+
+//             inpCheckboxArr.forEach(el => {
+//                 if(el.checked) {
+//                     characterTypes.push(el.dataset.characterType);
+//                 }
+//             })
+
+
+//             for(let l = 0; l < passwordLength; l++) {
+//                 let randomFunction = Math.floor(Math.random() * characterTypes.length);
+
+//                 functions.forEach(el => {
+//                     if(el.name.includes(characterTypes[randomFunction])) {
+//                         passwordLetters.push(el())
+//                     }
+//                 })
+//             }
+
+//             if(characterTypes.length === new Set(usedFunctions).size) {
+//                 passwordIsGenerated = true;
+//                     password = passwordLetters.join(``);
+//                     console.log(password)
+//                     console.log(characterTypes);
+//                     console.log(new Set(usedFunctions));
+//                     console.log(passwordIsGenerated)
+//                     passwordText.textContent = password;
+
+//                 inpCheckboxArr.forEach(el => el.checked = false)
+
+//             } else {
+//                 passwordIsGenerated = false;
+//             }
+
+//             password = passwordLetters.join(``);
+
+//             while(!passwordIsGenerated) {
+//                 characterTypes = [];
+//                 passwordLetters = [];
+//                 password = ``;
+//                 passwordIsGenerated = false;
+//                 usedFunctions = [];
+
+//                 inpCheckboxArr.forEach(el => {
+//                     if(el.checked) {
+//                         characterTypes.push(el.dataset.characterType);
+//                     }
+//                 })
+
+
+//                 for(let l = 0; l < passwordLength; l++) {
+//                     let randomFunction = Math.floor(Math.random() * characterTypes.length);
+
+//                     functions.forEach(el => {
+//                         if(el.name.includes(characterTypes[randomFunction])) {
+//                             passwordLetters.push(el())
+//                         }
+//                     })
+//                 }
+
+//                 if(characterTypes.length === new Set(usedFunctions).size) {
+//                     passwordIsGenerated = true;
+//                     // password = passwordLetters.join(``);
+//                     // console.log(password)
+//                     // console.log(characterTypes);
+//                     // console.log(new Set(usedFunctions));
+//                     // console.log(passwordIsGenerated)
+//                     // passwordText.textContent = password;
+//                 } else {
+//                     passwordIsGenerated = false;
+//                 }
+
+//             }
+//         }
+        
+//     }
+
+
+// })
+
+
+
+
+
+
+
 `use strict`;
 
 const inpRange = document.querySelector(`#inp-range`);
@@ -12,169 +200,51 @@ const passwordText = document.querySelector(`.password`);
 
 
 
-let characterTypes = [];
-let passwordLength;
 
-let passwordLetters = [];
+class AppPasswordGenerator {
 
-let password;
+    #characterTypes= [];
+    #passwordLength;
+    #passwordLetters = [];
+    #password; 
+    #passwordIsGenerated = false;
+    #usedFunctions= [];
 
-let passwordIsGenerated = false
-let usedFunctions= [];
-
-
-
-
-
-const updateSlider = function () {
-    const value = (inpRange.value - inpRange.min) / (inpRange.max - inpRange.min) * 100;
-    inpRange.style.background = `linear-gradient(to right, #A4FFAF 0%, #A4FFAF ${value}%, #18171F ${value}%, #18171F 100%)`;
-    passwordLength = inpRange.value;
-
-    spanCharacterLength.textContent = `${passwordLength}`
-}
-
-inpRange.addEventListener(`input`, updateSlider);
-
-updateSlider();
+    constructor() {
+        this.#submitForm();
+        this.#updateSlider();
+    };
 
 
-//// generates random symbol
-const getRandomSymbol = () => {
+    #updateSlider() {
+        this.#updateSliderBackground();
+        this.#updateRangeInput();
+    }
 
-    let s = `s`;
-    usedFunctions.push(s)
-  const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-  const randomIndex = Math.floor(Math.random() * symbols.length);
-  
-  return symbols[randomIndex];
-};
-
-///// generates random number
-const getRandomNumber = () => {
-    let n = `n`;
-    usedFunctions.push(n)
-    return Math.floor(Math.random() * 10)
-};
-
-
-/// generates random letter
-const getRandomLowercaseLetter = () => {
-    let l = `l`;
-    usedFunctions.push(l)
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  return alphabet[Math.floor(Math.random() * alphabet.length)];
-};
-
-
-///generate random uppercase letter 
-const getRandomUppercaseLetter = () => {
-    let u = `u`;
-    usedFunctions.push(u)
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  return alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
-};
-
-
-
-const  functions = [getRandomUppercaseLetter, getRandomLowercaseLetter, getRandomNumber, getRandomSymbol]
-
-
-
-
-btnSubmit.addEventListener(`click`, function(e) {
-    e.preventDefault();
-
-    if(passwordLength < 8) {
-        console.log(`bye`)
-    } else {
-
-        const inpIsChewcked = inpCheckboxArr.some(el => el.checked);
-
-        if(!inpIsChewcked) {
-            console.log(`not checked`)
-        } else {
-            characterTypes = [];
-            passwordLetters = [];
-            password = ``;
-            passwordIsGenerated = false;
-            usedFunctions = [];
-
-            inpCheckboxArr.forEach(el => {
-                if(el.checked) {
-                    characterTypes.push(el.dataset.characterType);
-                }
-            })
-
-
-            for(let l = 0; l < passwordLength; l++) {
-                let randomFunction = Math.floor(Math.random() * characterTypes.length);
-
-                functions.forEach(el => {
-                    if(el.name.includes(characterTypes[randomFunction])) {
-                        passwordLetters.push(el())
-                    }
-                })
-            }
-
-            if(characterTypes.length === new Set(usedFunctions).size) {
-                passwordIsGenerated = true;
-                    password = passwordLetters.join(``);
-                    console.log(password)
-                    console.log(characterTypes);
-                    console.log(new Set(usedFunctions));
-                    console.log(passwordIsGenerated)
-                    passwordText.textContent = password;
-
-                inpCheckboxArr.forEach(el => el.checked = false)
-
-            } else {
-                passwordIsGenerated = false;
-            }
-
-            password = passwordLetters.join(``);
-
-            while(!passwordIsGenerated) {
-                characterTypes = [];
-                passwordLetters = [];
-                password = ``;
-                passwordIsGenerated = false;
-                usedFunctions = [];
-
-                inpCheckboxArr.forEach(el => {
-                    if(el.checked) {
-                        characterTypes.push(el.dataset.characterType);
-                    }
-                })
-
-
-                for(let l = 0; l < passwordLength; l++) {
-                    let randomFunction = Math.floor(Math.random() * characterTypes.length);
-
-                    functions.forEach(el => {
-                        if(el.name.includes(characterTypes[randomFunction])) {
-                            passwordLetters.push(el())
-                        }
-                    })
-                }
-
-                if(characterTypes.length === new Set(usedFunctions).size) {
-                    passwordIsGenerated = true;
-                    // password = passwordLetters.join(``);
-                    // console.log(password)
-                    // console.log(characterTypes);
-                    // console.log(new Set(usedFunctions));
-                    // console.log(passwordIsGenerated)
-                    // passwordText.textContent = password;
-                } else {
-                    passwordIsGenerated = false;
-                }
-
-            }
-        }
-        
+    #updateRangeInput() {
+        inpRange.addEventListener(`input`, ()=> {
+            this.#updateSliderBackground();
+            this.#passwordLength = inpRange.value;
+            spanCharacterLength.textContent = `${this.#passwordLength}`;
+        })
+    }
+    
+    #updateSliderBackground() {
+        let value = (inpRange.value - inpRange.min) / (inpRange.max - inpRange.min) * 100;
+        inpRange.style.background = `linear-gradient(to right, #A4FFAF 0%, #A4FFAF ${value}%, #18171F ${value}%, #18171F 100%)`;
     }
 
 
-})
 
+    #submitForm() {
+        btnSubmit.addEventListener(`click`, function() {
+            console.log(`hi`)
+        })
+    }
+
+
+}
+
+const PasswordGenerator = new AppPasswordGenerator();
+
+// PasswordGenerator();
